5fc663991047d31dce49790091b19f566baa2aec
Angelica <Angelica137@users.noreply.github.com>
2020-09-07 14:24:09 +0100

    install flask restful and test item endpoint

--- app.py
+++ app.py
@@ -1,69 +1,26 @@
-from flask import Flask, jsonify, request, render_template
+from flask import Flask
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
 
-stores= [
-	{
-		'name': 'My store',
-		'items': [
-			{
-				'name': 'Item',
-				'price': 2.80
-			}
-		]
-	}
-]
+items = []
 
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+class Item(Resource):
+    def get(self, name):
+        for item in items:
+            if item['name'] == name:
+                return item
+
+    def post(self, name):
+        item = {'name': name, 'price': 12.00}
+        items.append(item)
+        return item
 
 
-@app.route('/sotre', methods=['POST'])
-def create_store():
-    request_data = request.get_json()
-    new_store = {
-			'name': request_data['name'],
-			'items': []
-		}
-    stores.append(new_store)
-    return jsonify(new_store)
+api.add_resource(Item, '/item/<string:name>')
 
 
-@app.route('/store/<string:name>')
-def get_store(name):
-    for store in stores:
-        if store['name'] == name:
-              return jsonify(store)
-    return jsonify({'message': 'store not found'})
-
-
-@app.route('/store')
-def get_stores():
-    return jsonify({'stores': stores})
-
-
-@app.route('/store/<string:name>/item', methods=['POST'])
-def create_item_in_store(name):
-    request_data = request.get_json()
-    for store in stores:
-        if store['name'] == name:
-            new_item = {
-                'name': request_data['name'],
-								'price': request_data['price']
-						}
-            store['items'].append(new_item)
-            return jsonify(new_item)
-    return jsonify({'message': 'store not found'})
-
-
-@app.route('/store/<string:name>/item')
-def get_items_in_store(name):
-    for store in stores:
-        if store['name'] == name:
-            return jsonify({'items': store['items']})
-    return jsonify({'message': 'store not found'})
+app.run(port=5000)
