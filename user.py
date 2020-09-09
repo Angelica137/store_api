98f4daed35838717e34dc14db2a271ed6d63acc5
Angelica <Angelica137@users.noreply.github.com>
2020-09-09 18:04:31 +0100

    add suer register

--- user.py
+++ user.py
@@ -1,5 +1,13 @@
+import squlite3
+from flask_restful import Resource
+
 class User:
     def __init__(self, _id, username, password):
         self.id = _id
         self.username = username
         self.password = password
+
+
+class UserRegister(Resource):
+    def post(self):
+        pass
