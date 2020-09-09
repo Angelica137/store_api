4846a5f5ab23700bacfe0e0034890333e1fa08c2
Angelica <Angelica137@users.noreply.github.com>
2020-09-09 11:27:01 +0100

    add security.py

---
+++ security.py
@@ -0,0 +1,19 @@
+users = [
+	{
+		'id': 1,
+		'username': 'bob',
+		'pasword': 'asdf'
+	}
+]
+
+user_mapping = { 'bob': {
+	'id': 1,
+	'username': 'bob',
+	'password': 'asdf'
+}}
+
+userid_mapping = { 1: {
+	'id': 1,
+	'username': 'bob',
+	'password': 'asdf'
+}}
