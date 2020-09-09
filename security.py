05ba537e561baa2e1197a3fe05ae23ae72667037
Angelica <Angelica137@users.noreply.github.com>
2020-09-09 11:30:31 +0100

    add authenticate  and identity fucntoins

--- security.py
+++ security.py
@@ -17,3 +17,13 @@ userid_mapping = { 1: {
 	'username': 'bob',
 	'password': 'asdf'
 }}
+
+
+def authenticate(username, password):
+    user = username_mapping.get(username, None)
+    if user and user.password == password:
+        return user
+
+def identity(payload):
+    user_id = payload ['identity']
+    return useris_mapping.get(user_id, None)
