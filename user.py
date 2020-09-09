d4aa27caf9042fae517be81c7bf218ee44ddb48b
Angelica <Angelica137@users.noreply.github.com>
2020-09-09 18:33:07 +0100

    add user mapping to user _id

--- user.py
+++ user.py
@@ -7,15 +7,32 @@ class User:
         self.username = username
         self.password = password
 
+    @classmethod
     def find_by_username(self, username):
         connection = sqlite3.connect('data.db')
         sursor = connection.cursor()
 
         query = "SELECT * FROM users WHERE username=?"
         result = curson.execute(query, (username,))
-				row = result.fetchone()
-				if row:
-            user = User(rpw[0], row[1], row[2])
+        row = result.fetchone()
+        if row:
+            user = cls(*row)
+        else:
+            user = None
+
+        connection.close()
+        return user
+
+    @classmethod
+    def find_by_id(self, _id):
+        connection = sqlite3.connect('data.db')
+        sursor = connection.cursor()
+
+        query = "SELECT * FROM users WHERE id=?"
+        result = curson.execute(query, (_id,))
+        row = result.fetchone()
+        if row:
+            user = cls(*row)
         else:
             user = None
 
