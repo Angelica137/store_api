70135f1f92f583c21c3cc29d89423e620f59d9bc
Angelica <Angelica137@users.noreply.github.com>
2020-09-12 18:49:14 +0100

    add item rel to stores.py

--- models/store.py
+++ models/store.py
@@ -4,8 +4,10 @@ import db from db
 class StoreModel(db.Model):
     __tablename__ = 'stores'
 
-    id - db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
+
+		items = db.relationship('ItemModel')
 
     def __init__(self, name):
         self.name = name
