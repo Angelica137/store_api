99e8a3dabf3c31e211a99f5dd7b251abf39228b0
Angelica <Angelica137@users.noreply.github.com>
2020-09-12 14:59:41 +0100

    amend items table name in item model

--- models/item.py
+++ models/item.py
@@ -2,8 +2,7 @@ from db import db
 
 
 class ItemModel(db.Model):
-
-    __tablename__ = 'item'
+    __tablename__ = 'items'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
