0e8b336ae94a0fe0e68bd0aa3b7b39019d4c0d44
Angelica <Angelica137@users.noreply.github.com>
2020-09-12 12:37:23 +0100

    create item model

--- resources/item.py
+++ resources/item.py
@@ -14,19 +14,6 @@ class Item(Resource):
         if item:
             return item
         return {'message': 'Item not found'}, 404
-
-    @classmethod
-    def find_by_name(cls, name):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "SELECT * FROM items WHERE name=?"
-        result = cursor.execute(query, (name, ))
-        row = result.fetchone()
-        connection.close()
-
-        if row:
-            return {'item': {'name': row[0], 'price': row[1]}}
     
     def post(self, name):        
         if self.find_by_name(name):
@@ -42,17 +29,6 @@ class Item(Resource):
             return {"message": "An error occured insterting the item."}, 500
 
         return item, 201
-
-    @classmethod
-    def insert(cls, item):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-        
-        query = "INSERT INTO items VALUES (?, ?)"
-        cursor.execute(query, (item['name'], item['price']))
-
-        connection.commit()
-        connection.close()
 
     def delete(self, name):
         connection = sqlite3.connect('data.db')
@@ -84,19 +60,6 @@ class Item(Resource):
             except:
                 return {"message": "An error occured updating the item"}, 500
         return updated_item
-
-    @classmethod
-    def update(cls, item):
-        connection = sqlite3.connect('data.db')
-
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-        
-        query = "UPDATE items SET price=? WHERE name=?"
-        cursor.execute(query, (item['price'], item['name']))
-
-        connection.commit()
-        connection.close()
 
 
 class ItemList(Resource):
