aab9747e3d95e69c30c9a0253ef9f241f5c2c048
Angelica <Angelica137@users.noreply.github.com>
2020-09-12 14:12:04 +0100

    override default table name users

--- models/user.py
+++ models/user.py
@@ -3,6 +3,7 @@ from db import db
 
 
 class UserModel(db.Model):
+    __tablename__ = 'users'
     def __init__(self, _id, username, password):
         self.id = _id
         self.username = username
