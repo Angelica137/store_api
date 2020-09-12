69087d7b54b8aa430bf3f7e2375d984e11502fc8
Angelica <Angelica137@users.noreply.github.com>
2020-09-12 17:30:30 +0100

    refactory .find_by_id

--- models/user.py
+++ models/user.py
@@ -19,16 +19,4 @@ class UserModel(db.Model):
 
     @classmethod
     def find_by_id(cls, _id):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "SELECT * FROM users WHERE id=?"
-        result = cursor.execute(query, (_id,))
-        row = result.fetchone()
-        if row:
-            user = cls(*row)
-        else:
-            user = None
-
-        connection.close()
-        return user
+        return cls.query.filter_by(id=_id).first()
