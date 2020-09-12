87c49d8b05c4a10855bed19234305148dc41718e
Angelica <Angelica137@users.noreply.github.com>
2020-09-12 13:19:11 +0100

    fix item.py typos

--- models/user.py
+++ models/user.py
@@ -26,10 +26,10 @@ class UserModel:
     @classmethod
     def find_by_id(cls, _id):
         connection = sqlite3.connect('data.db')
-        sursor = connection.cursor()
+        cursor = connection.cursor()
 
         query = "SELECT * FROM users WHERE id=?"
-        result = curson.execute(query, (_id,))
+        result = cursor.execute(query, (_id,))
         row = result.fetchone()
         if row:
             user = cls(*row)
