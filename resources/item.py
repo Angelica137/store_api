41718325e313c3a3e8e5352093cc4cb6682b85b3
Angelica <Angelica137@users.noreply.github.com>
2020-09-12 12:40:53 +0100

    refactor item resource

--- resources/item.py
+++ resources/item.py
@@ -1,6 +1,7 @@
 import sqlite3
 from flask_restful import Resource, reqparse
 from flask_jwt import jwt_required
+from models.item import ItemModel
 
 
 
@@ -10,13 +11,13 @@ class Item(Resource):
     
     @jwt_required()
     def get(self, name):
-        item = self.find_by_name(name)
+        item = ItemModel.find_by_name(name)
         if item:
             return item
         return {'message': 'Item not found'}, 404
     
     def post(self, name):        
-        if self.find_by_name(name):
+        if ItemModel.find_by_name(name):
             return {'message': "An item with name '{}' already exists".format(name)}, 400
         
         data = Item.parser.parse_args()
@@ -24,7 +25,7 @@ class Item(Resource):
         item = {'name': name, 'price': data['price']}
 
         try:
-            self.insert(item)
+            ItemModel.insert(item)
         except:
             return {"message": "An error occured insterting the item."}, 500
 
@@ -45,18 +46,18 @@ class Item(Resource):
     def put(self, name):
         data = Item.parser.parse_args()
 
-        item = self.find_by_name(name)
+        item = ItemModel.find_by_name(name)
         updated_item = {'name': name, 'price': data['price']}
         
         if item is None:
             try:
-                seilf.insert(update_item)
+                ItemModel.insert(update_item)
             except:
                 return {"message": "An error occured inserting the item."}, 500
 
         else:
             try:
-                self.update(updated_item)
+                ItemModel.update(updated_item)
             except:
                 return {"message": "An error occured updating the item"}, 500
         return updated_item
